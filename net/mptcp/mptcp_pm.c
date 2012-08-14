eef88cc807b3c524dd548f24a4fc125847f53f27
Raphael Wutzke <raphael.wutzke@googlemail.com>
2012-08-14 17:34:51 +0300

    Build-Fix: Backporting problems Signed-off-by: Raphael Wutzke <raphael.wutzke@googlemail.com>

--- net/mptcp/mptcp_pm.c
+++ net/mptcp/mptcp_pm.c
@@ -27,7 +27,6 @@
  *      2 of the License, or (at your option) any later version.
  */
 
-#include <linux/kconfig.h>
 #include <linux/module.h>
 #include <linux/netdevice.h>
 #include <linux/inetdevice.h>
