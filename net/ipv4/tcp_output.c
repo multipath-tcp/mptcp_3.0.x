b51f51a4e41a109d9c89025c9d13b46d46a590e5
Christoph Paasch <christoph.paasch@uclouvain.be>
2012-10-24 10:04:15 +0200

    Fix: Copy mptcp_flags in tcp_fragment
    Signed-off-by: Christoph Paasch <christoph.paasch@uclouvain.be>
    Signed-off-by: Gregory Detal <gregory.detal@uclouvain.be>

--- net/ipv4/tcp_output.c
+++ net/ipv4/tcp_output.c
@@ -1076,6 +1076,8 @@ int tcp_fragment(struct sock *sk, struct sk_buff *skb, u32 len,
 	TCP_SKB_CB(skb)->flags = flags & ~(TCPHDR_FIN | TCPHDR_PSH);
 	TCP_SKB_CB(buff)->flags = flags;
 	TCP_SKB_CB(buff)->sacked = TCP_SKB_CB(skb)->sacked;
+	if (tp->mpc)
+		mptcp_fragment(skb, buff);
 
 	if (!skb_shinfo(skb)->nr_frags && skb->ip_summed != CHECKSUM_PARTIAL) {
 		/* Copy and checksum data tail into the new buffer. */
