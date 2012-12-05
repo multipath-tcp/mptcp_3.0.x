b8466ad50bb1f16502a1a92886ec87e96955c03c
Christoph Paasch <christoph.paasch@uclouvain.be>
2012-12-05 10:24:27 +0100

    Fix Build-warning
    /home/christoph/workspace/linux/mtcp_2.6.36/net/mptcp/mptcp_pm.c: In
    function ‘mptcp_pm_init’:
    /home/christoph/workspace/linux/mtcp_2.6.36/net/mptcp/mptcp_pm.c:1086:1:
    warning: label ‘mptcp_pm_v6_failed’ defined but not used
    [-Wunused-label]
    
    Signed-off-by: Christoph Paasch <christoph.paasch@uclouvain.be>
    Signed-off-by: Gregory Detal <gregory.detal@uclouvain.be>

--- net/mptcp/mptcp_pm.c
+++ net/mptcp/mptcp_pm.c
@@ -1028,7 +1028,7 @@ static const struct file_operations mptcp_pm_seq_fops = {
 	.release = single_release_net,
 };
 
-static __net_init int mptcp_pm_proc_init_net(struct net *net)
+static int mptcp_pm_proc_init_net(struct net *net)
 {
 	if (!proc_net_fops_create(net, "mptcp", S_IRUGO, &mptcp_pm_seq_fops))
 		return -ENOMEM;
@@ -1036,12 +1036,12 @@ static __net_init int mptcp_pm_proc_init_net(struct net *net)
 	return 0;
 }
 
-static __net_exit void mptcp_pm_proc_exit_net(struct net *net)
+static void mptcp_pm_proc_exit_net(struct net *net)
 {
 	proc_net_remove(net, "mptcp");
 }
 
-static __net_initdata struct pernet_operations mptcp_pm_proc_ops = {
+static struct pernet_operations mptcp_pm_proc_ops = {
 	.init = mptcp_pm_proc_init_net,
 	.exit = mptcp_pm_proc_exit_net,
 };
@@ -1081,8 +1081,9 @@ out:
 mptcp_pm_v4_failed:
 #if defined(CONFIG_IPV6) || defined(CONFIG_IPV6_MODULE)
 	mptcp_pm_v6_undo();
-#endif
+
 mptcp_pm_v6_failed:
+#endif
 #ifdef CONFIG_SYSCTL
 	unregister_pernet_subsys(&mptcp_pm_proc_ops);
 #endif
