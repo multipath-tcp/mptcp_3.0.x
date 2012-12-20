e613184283454af64ec3b6759700810af58ab8c9
Gregory Detal <gregory.detal@uclouvain.be>
2012-12-20 15:29:17 +0100

    Cleanup
    Signed-off-by: Gregory Detal <gregory.detal@uclouvain.be>
    Signed-off-by: Christoph Paasch <christoph.paasch@uclouvain.be>
    Signed-off-by: Gregory Detal <gregory.detal@uclouvain.be>

--- net/mptcp/mptcp_output.c
+++ net/mptcp/mptcp_output.c
@@ -1268,9 +1268,8 @@ void mptcp_established_options(struct sock *sk, struct sk_buff *skb,
 	    ((mpcb->send_infinite_mapping && tcb &&
 	      !(tcb->mptcp_flags & MPTCPHDR_INF) &&
 	      !before(tcb->seq, tp->mptcp->infinite_cutoff_seq)) ||
-	     !mpcb->send_infinite_mapping)) {
+	     !mpcb->send_infinite_mapping))
 		return;
-	}
 
 	if (unlikely(tp->mptcp->include_mpc)) {
 		opts->options |= OPTION_MPTCP;
@@ -1371,7 +1370,6 @@ void mptcp_established_options(struct sock *sk, struct sk_buff *skb,
 
 	if (skb)
 		tp->mptcp->include_mpc = 0;
-
 	return;
 }
 
