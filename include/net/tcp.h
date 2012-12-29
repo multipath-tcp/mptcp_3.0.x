85a240a7b83581fb169524f2f669160f095043d2
Christoph Paasch <christoph.paasch@uclouvain.be>
2012-12-29 13:18:14 +0100

    Cleanup
    Signed-off-by: Christoph Paasch <christoph.paasch@uclouvain.be>
    
    Conflicts:
    	include/net/tcp.h
    	net/ipv4/tcp_input.c
    
    Signed-off-by: Gregory Detal <gregory.detal@uclouvain.be>

--- include/net/tcp.h
+++ include/net/tcp.h
@@ -430,7 +430,7 @@ extern int tcp_ioctl(struct sock *sk, int cmd, unsigned long arg);
 extern int tcp_rcv_state_process(struct sock *sk, struct sk_buff *skb,
 				 struct tcphdr *th, unsigned len);
 extern int tcp_rcv_established(struct sock *sk, struct sk_buff *skb,
-			       struct tcphdr *th, unsigned len);
+			       struct tcphdr *th, unsigned int len);
 extern void tcp_rcv_space_adjust(struct sock *sk);
 extern void tcp_cleanup_rbuf(struct sock *sk, int copied);
 extern int tcp_twsk_unique(struct sock *sk, struct sock *sktw, void *twp);
