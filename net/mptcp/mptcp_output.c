knbnaz/android_kernel_lge_sm8150

Fix: Don't reinject if meta is closed
In that case we passed already via tcp_v4_destroy_sock and thus purged
the reinject-queue. It won't get purged again, and we would have a
memory-leak if we add a segment to the reinject-queue.

So, don't reinject if the meta is in TCP_CLOSE.

Further, don't reinject empty subflow fins with a data-fin. They are
reinjected by the code at the end of mptcp_reinject_data, without the
tcp_flag on TCPHDR_FIN.

Signed-off-by: Christoph Paasch <christoph.paasch@uclouvain.be>

net/mptcp/mptcp_output.c
@@ -290,12 +290,20 @@ void mptcp_reinject_data(struct sock *sk, int clone_it)
 	struct tcp_sock *tp = tcp_sk(sk);
 	struct sock *meta_sk = tp->meta_sk;
 
+	/* It has already been closed - there is really no point in reinjecting */
+	if (meta_sk->sk_state == TCP_CLOSE)
+		return;
+
 	skb_queue_walk_safe(&sk->sk_write_queue, skb_it, tmp) {
 		struct tcp_skb_cb *tcb = TCP_SKB_CB(skb_it);
-		/* Subflow syn's and fin's are not reinjected
+		/* Subflow syn's and fin's are not reinjected.
+		 *
+		 * As well as empty subflow-fins with a data-fin.
+		 * They are reinjected below (without the subflow-fin-flag)
 		 */
 		if (tcb->tcp_flags & TCPHDR_SYN ||
-		    (tcb->tcp_flags & TCPHDR_FIN && !mptcp_is_data_fin(skb_it)))
+		    (tcb->tcp_flags & TCPHDR_FIN && !mptcp_is_data_fin(skb_it)) ||
+		    (tcb->tcp_flags & TCPHDR_FIN && mptcp_is_data_fin(skb_it) && !skb_it->len))
 			continue;
 
 		/* Go to next segment, if it failed */

multipath-tcp/mptcp_3.0.x

Conflicts:
	net/mptcp/mptcp_output.c
Signed-off-by: Gregory Detal <gregory.detal@uclouvain.be>

net/mptcp/mptcp_output.c
@@ -289,12 +289,20 @@ void mptcp_reinject_data(struct sock *sk, int clone_it)
 	struct tcp_sock *tp = tcp_sk(sk);
 	struct sock *meta_sk = tp->meta_sk;
 
+	/* It has already been closed - there is really no point in reinjecting */
+	if (meta_sk->sk_state == TCP_CLOSE)
+		return;
+
 	skb_queue_walk_safe(&sk->sk_write_queue, skb_it, tmp) {
 		struct tcp_skb_cb *tcb = TCP_SKB_CB(skb_it);
-		/* Subflow syn's and fin's are not reinjected
+		/* Subflow syn's and fin's are not reinjected.
+		 *
+		 * As well as empty subflow-fins with a data-fin.
+		 * They are reinjected below (without the subflow-fin-flag)
 		 */
 		if (tcb->flags & TCPHDR_SYN ||
-		    (tcb->flags & TCPHDR_FIN && !mptcp_is_data_fin(skb_it)))
+		    (tcb->flags & TCPHDR_FIN && !mptcp_is_data_fin(skb_it)) ||
+		    (tcb->flags & TCPHDR_FIN && mptcp_is_data_fin(skb_it) && !skb_it->len))
 			continue;
 
 		/* Go to next segment, if it failed */