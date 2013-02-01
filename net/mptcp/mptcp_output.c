2912b4ca4bfb281953fcbbcfc53f2631f8d365c3
Christoph Paasch <christoph.paasch@uclouvain.be>
2013-02-01 20:38:30 +0100

    Fix: Only send reset if necessary
    Because, the socket may not be correctly initialized (e.g.,
    mptcp_init4_subsockets failed). Then, rcv_mss is 0, and
    __mptcp_select_window will fail with a 0 divide (called from
    tcp_transmit_skb).
    
    Signed-off-by: Christoph Paasch <christoph.paasch@uclouvain.be>
    
    Conflicts:
    	net/ipv4/tcp.c
    
    Signed-off-by: Gregory Detal <gregory.detal@uclouvain.be>

--- net/mptcp/mptcp_output.c
+++ net/mptcp/mptcp_output.c
@@ -1747,7 +1747,8 @@ found:
 			continue;
 
 		sk_it->sk_err = ECONNRESET;
-		tcp_send_active_reset(sk_it, GFP_ATOMIC);
+		if (tcp_need_reset(sk_it->sk_state))
+			tcp_send_active_reset(sk_it, GFP_ATOMIC);
 		mptcp_sub_force_close(sk_it);
 	}
 	if (!in_serving_softirq())
