841ad220b7ddc44f0228a50ec802e96524f249b5
Christoph Paasch <christoph.paasch@uclouvain.be>
2013-02-11 22:22:57 +0100

    Fix: Don't use the teardown-flag
    We should do a proper close with mptcp_sub_force_close.
    
    Signed-off-by: Christoph Paasch <christoph.paasch@uclouvain.be>
    
    Conflicts:
    	net/ipv4/tcp_ipv4.c
    
    Signed-off-by: Gregory Detal <gregory.detal@uclouvain.be>

--- net/ipv4/tcp_ipv4.c
+++ net/ipv4/tcp_ipv4.c
@@ -687,9 +687,12 @@ void tcp_v4_send_reset(struct sock *sk, struct sk_buff *skb)
 	TCP_INC_STATS_BH(net, TCP_MIB_OUTSEGS);
 	TCP_INC_STATS_BH(net, TCP_MIB_OUTRSTS);
 
-#ifdef CONFIG_MPTCP
-	if (sk && tcp_sk(sk)->mpc && tcp_sk(sk)->mptcp->teardown)
-		tcp_done(sk);
+#ifdef CONFIG_TCP_MD5SIG
+release_sk1:
+	if (sk1) {
+		rcu_read_unlock();
+		sock_put(sk1);
+	}
 #endif
 }
 
